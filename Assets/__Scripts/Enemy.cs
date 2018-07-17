d03b474e941a07b97d71783a5c450ec59cb43f53
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-07-16 23:13:50 -0400

    Dungeon Delver: set up cameras, prepared delvertiles, created initial scripts, dray/skeletos sprites , movement of dray. Minor fixes need to be made - help on Dray sprites!

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Enemy : MonoBehaviour { 	protected static Vector3[] directions = new Vector3[] { 		Vector3.right, Vector3.up, Vector3.left, Vector3.down 	} ;  	[Header("Set in Inspector: Enemy")] 	public float maxHealth = 1;  	[Header("Set Dynamically: Enemy")] 	public float health;  	protected Animator anim; 	protected Rigidbody rigid; 	protected SpriteRenderer sRend;  	protected virtual void Awake() { 		health = maxHealth; 		anim = GetComponent<Animator>(); 		rigid = GetComponent<Rigidbody>(); 		sRend = GetComponent<SpriteRenderer>(); 	}       
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
